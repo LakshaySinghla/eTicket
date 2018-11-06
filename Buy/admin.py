625059cb228c20b2f605ae6aaa7c2aa40487245e
LakshaySinghla <lukkysinghla@gmail.com>
2018-11-06 19:36:51 +0530

    Models:Stadium,User,Match,RemainingTicket,Ticket,TicketType

--- Buy/admin.py
+++ Buy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from Buy.models import User, Stadium, Ticket, TicketType, RemainingTickets,Match
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Stadium)
+admin.site.register(Ticket)
+admin.site.register(TicketType)
+admin.site.register(RemainingTickets)
+admin.site.register(Match)
--- Buy/models.py
+++ Buy/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	name = models.CharField(max_length=50)
+	emailId = models.CharField(max_length=50,unique=True)
+	password = models.CharField(max_length=50)
+	def __str__(self):
+		return self.name
+
+class Stadium(models.Model):
+	name = models.CharField(max_length=256)
+	location = models.CharField(max_length=256)
+	typeATotal = models.IntegerField(default=0)
+	typeBTotal = models.IntegerField(default=0)
+	typeCTotal = models.IntegerField(default=0)
+	typeAPrice = models.IntegerField(default=0)
+	typeBPrice = models.IntegerField(default=0)
+	typeCPrice = models.IntegerField(default=0)
+	def __str__(self):
+		return self.name
+	
+class RemainingTickets(models.Model):
+	stadiumId = models.ForeignKey(Stadium, on_delete=models.CASCADE)
+	typeARemaining = models.IntegerField(default=0)
+	typeBRemaining = models.IntegerField(default=0)
+	typeCRemaining = models.IntegerField(default=0)
+	
+	def update(self, stadium):
+		self.typeARemaining = stadium.typeATotal 
+		self.typeBRemaining = stadium.typeBTotal
+		self.typeCRemaining = stadium.typeCTotal
+	
+	def __str__(self):
+		return self.stadiumId.name
+
+class Match(models.Model):
+	teamA = models.CharField(max_length=256)
+	teamB = models.CharField(max_length=256)
+	stadiumId = models.ForeignKey(Stadium,on_delete=models.CASCADE)
+	date = models.DateTimeField('match_date')
+	def __str__(self):
+		return self.teamA+' vs '+self.teamB
+
+
+class TicketType(models.Model):
+	name = models.CharField(max_length=1)
+	price = models.IntegerField(default=0)
+	def __str__(self):
+		return self.name + ' '+ str(self.price)
+
+class Ticket(models.Model):
+	ticketType = models.ForeignKey(TicketType, on_delete=models.CASCADE)
+	emailId = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+	matchId = models.ForeignKey(Match, on_delete=models.CASCADE, default=0)
+	def __str__(self):
+		return self.ticketType.name
